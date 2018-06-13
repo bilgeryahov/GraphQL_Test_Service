d5744bba3b5d083be15669ca09f36f70d1b98a55
Bilger Yahov <bayahov1@gmail.com>
2018-06-13 14:29:51 +0200

    Add three more books.

--- GraphQL_Test_Service/server/schema/schema.js
+++ GraphQL_Test_Service/server/schema/schema.js
@@ -12,7 +12,10 @@ const {
 const books = [
 	{ name: 'Name of the Wind', genre: 'Fantasy', id: 1, authorID: 1 },
 	{ name: 'The Final Empire', genre: 'Fantasy', id: 2, authorID: 2 },
-	{ name: 'The Long Earth', genre: 'Sci-Fi', id: 3, authorID: 3 }
+	{ name: 'The Long Earth', genre: 'Sci-Fi', id: 3, authorID: 3 },
+	{ name: 'The Hero of Ages', genre: 'Fantasy', id: 4, authorID: 2 },
+	{ name: 'The Colour of Magic', genre: 'Fantasy', id: 5, authorID: 3 },
+	{ name: 'The Light Fantastic', genre: 'Fantasy', id: 6, authorID: 3 }
 ];
 
 const authors = [
