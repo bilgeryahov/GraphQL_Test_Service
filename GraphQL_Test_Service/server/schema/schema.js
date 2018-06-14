ef2e6096548280ae7a3a5d7c0a06ccf44531be94
Bilger Yahov <bayahov1@gmail.com>
2018-06-14 11:03:28 +0200

    Add Mutations. Start with adding a new author.

--- GraphQL_Test_Service/server/schema/schema.js
+++ GraphQL_Test_Service/server/schema/schema.js
@@ -74,6 +74,28 @@ const RootQuery = new GraphQLObjectType({
 	}
 });
 
+const Mutation = new GraphQLObjectType({
+	name: 'Mutation',
+	fields: {
+		addAuthor: {
+			type: AuthorType,
+			args: {
+				name: { type: GraphQLString },
+				age: { type: GraphQLInt }
+			},
+			resolve: (parent, args) => {
+				let author = new Author({
+					name: args.name,
+					age: args.age
+				});
+				// The power of Mongoose... :P.
+				return author.save();
+			}
+		}
+	}
+});
+
 module.exports = new GraphQLSchema({
-	query: RootQuery
+	query: RootQuery,
+	mutation: Mutation
 });
