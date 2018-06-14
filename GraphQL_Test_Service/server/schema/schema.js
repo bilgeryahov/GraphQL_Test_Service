da8d73b20b6704d8b0d5b0805cdfbf978ff60b79
Bilger Yahov <bayahov1@gmail.com>
2018-06-14 12:44:07 +0200

    Make sure that the arguments cannot be null.

--- GraphQL_Test_Service/server/schema/schema.js
+++ GraphQL_Test_Service/server/schema/schema.js
@@ -10,7 +10,8 @@ const {
 	GraphQLInt,
 	GraphQLID,
 	GraphQLList,
-	GraphQLSchema
+	GraphQLSchema,
+	GraphQLNonNull
 } = graphql;
 
 const BookType = new GraphQLObjectType({
@@ -83,8 +84,8 @@ const Mutation = new GraphQLObjectType({
 		addAuthor: {
 			type: AuthorType,
 			args: {
-				name: { type: GraphQLString },
-				age: { type: GraphQLInt }
+				name: { type: new GraphQLNonNull(GraphQLString) },
+				age: { type: new GraphQLNonNull(GraphQLInt) }
 			},
 			resolve: (parent, args) => {
 				let author = new Author({
@@ -98,9 +99,9 @@ const Mutation = new GraphQLObjectType({
 		addBook: {
 			type: BookType,
 			args: {
-				name: { type: GraphQLString },
-				genre: { type: GraphQLString },
-				authorID: { type: GraphQLID }
+				name: { type: new GraphQLNonNull(GraphQLString) },
+				genre: { type: new GraphQLNonNull(GraphQLString) },
+				authorID: { type: new GraphQLNonNull(GraphQLID) }
 			},
 			resolve: (parent, args) => {
 				let book = new Book({
